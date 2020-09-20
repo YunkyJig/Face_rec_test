0d6b0661c15b2a71ee3b94259f1e76070e7ab1ee
GhostUser <35362972+GhostUser@users.noreply.github.com>
2020-09-20 21:06:55 +0530

    Updated

--- encode_face.py
+++ encode_face.py
@@ -1,5 +1,3 @@
-# face recognition with openCv, python and deep learning
-# Importin the libraries
 from imutils import paths
 import face_recognition
 import argparse
@@ -15,7 +13,7 @@ ap.add_argument("-d", "--detection-method", type=str, default="cnn", help="face 
 args= vars(ap.parse_args)
 
 # grab the paths to the input images in our dataset
-print("[INFO] quantifying faces...")
+print("Quantifying faces...")
 imagePaths = list(paths.list_images("dataset"))
 # initialize the list of known encodings and known names
 knownEncodings = []
@@ -23,17 +21,13 @@ knownNames = []
 
 #loop over the image paths
 for (i, imagePath) in enumerate(imagePaths):
-    #extract the person name from the image path
-    print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
+    print("Processing image {}/{}".format(i + 1, len(imagePaths)))
     name = imagePath.split(os.path.sep)[-2]
 
-    # load the input image and convert it from BGR (OpenCV ordering)
-    # to dlib ordering (RGB)
+
     image = cv2.imread(imagePath)
     rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    #detect the (x,y) -coordinates of the bounding boxes
-    # corresponding to each face of the input image
     boxes= face_recognition.face_locations(rgb, model="detection_method")
 
     #compute the facial embedding for the face
@@ -41,14 +35,12 @@ for (i, imagePath) in enumerate(imagePaths):
 
     # loop over the encodings
     for encoding in encodings:
-        # add each encoding + name to our set of known names and
-        # encodings
         knownEncodings.append(encoding)
         knownNames.append(name)
 
 # Dump the facial encodings + names to disk
-print("[INFO] serializing encodings...")
+print("Serializing encodings.....")
 data = {"encodings": knownEncodings, "names": knownNames}
-f = open("encodings", "wb")
+f = open("encodings.pickle", "wb")
 f.write(pickle.dumps(data))
 f.close()
